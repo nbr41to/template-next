ca76fab2162cb7355fafe4af764d3e91fefe6f4a
nbr41to <nbr.41to@gmail.com>
2021-09-24 22:08:36 +0900

    .

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
+  plugins: ['stylelint-prettier'],
   extends: [
     'stylelint-config-standard',
     'stylelint-config-recess-order',
+    'stylelint-prettier/recommended',
     'stylelint-config-prettier',
-    'stylelint-config-prettier/recommended',
   ],
   ignoreFiles: '**/node_modules/**',
   rules: {
-    'prettier/prettier': true,
     'block-no-empty': null,
+    'property-no-unknown': true,
   },
 };
