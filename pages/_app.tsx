0b6cfca879d39274513d67d9745c501668c1e825
nbr41to <nbr.41to@gmail.com>
2021-09-24 23:15:15 +0900

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
-import type { AppProps /*, AppContext */ } from 'next/app';
-import { ThemeProvider } from 'styled-components';
-import { theme } from 'src/styles/theme';
-
 import 'src/styles/reset.css';
 import 'src/styles/globals.css';
+
+import type { AppProps /*, AppContext */ } from 'next/app';
+import { theme } from 'src/styles/theme';
+import { ThemeProvider } from 'styled-components';
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
